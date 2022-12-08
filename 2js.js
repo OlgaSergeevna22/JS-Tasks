3d2d96312cf6cd728ea699092480bc25eaa6fcd5
Olga Geres <olgageres13@gmail.com>
2022-12-09 01:41:59 +0300

    Task 9

--- 2js.js
+++ 2js.js
@@ -4,27 +4,27 @@
 // let country = 'Sweden';
 // let access;
 // if (country == 'Sweden') {
-// access = true;
-// } else{
-// access = false;
+//     access = true;
+// } else {
+//     access = false;
 // }
 
-// let country = 'Sweden';
-// let access = (country === 'Sweden') ? 'true' : 'false';
+let country = 'Sweden';
+let access = (country === 'Sweden') ? 'true' : 'false';
 
-// console.log(access);
+console.log(access);
 
 
 // Задание 2
 // Создать переменную и записать в нее число, например 10.
 // 10 раз увеличить значение этой переменной на 1. Результат (1 шт.) вывести в консоль.
 
-// let a = 10;
-// for (let i = 0; i < 10; i++) {
-//     a = a + 1;
-// }
+let a = 10;
+for (let i = 0; i < 10; i++) {
+    a = a + 1;
+}
 
-// console.log(a);
+console.log(a);
 
 
 // Задание 3
@@ -32,69 +32,67 @@
 // - запрашивать у пользователя ввод числа
 // - проверять, равно ли это число 10. Если равно, выводить "Равно 10". Иначе выводить "Не равно 10"
 
-// for (i = 0; i <= 8; i += 2) {
-//     let numberUser = +prompt("Введите число");
-//     if (numberUser === 10) {
-//         console.log('Равно 10');
-//     } else {
-//         console.log('Не равно 10');
-//     }
-// }
+for (let i = 0; i <= 8; i += 2) {
+    let numberUser = +prompt("Введите число");
+    if (numberUser === 10) {
+        console.log('Равно 10');
+    } else {
+        console.log('Не равно 10');
+    }
+}
 
 // Задание 4
 // Вывести в консоль квадраты чисел от 0 до 100 (не включая 100) - т.е. вывести числа 0, 1, 4, 9, 16...
 // * Задавать количество чисел, которые нужно вывести, при помощи функции prompt.
 
-// const count = +prompt('Введите количество чисел');
-// for (let i = 0; i < count; i++) {
-//     console.log(i ** 2);
-// }
+const count = +prompt('Введите количество чисел');
+for (let i = 0; i < count; i++) {
+    console.log(i ** 2);
+}
 
 // Задание 5
 // Напишите программу, которая выводит в консоль числа от 1 до 100.
 // При этом вместо чисел, кратных трем, программа должна выводить слово «Fizz», а вместо чисел, кратных пяти, — слово «Buzz». Если число кратно и 3, и 5, то программа должна выводить слово «FizzBuzz»
 
-// for (i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
-//     if (i % 3 === 0 && i % 5 === 0) {
-//         console.log('FizzBuzz');
-//     } else if (i % 5 === 0) {
-//         console.log('Buzz');
-//     } else if (i % 3 === 0) {
-//         console.log('Fizz');
-//     } else {
-//         console.log(i);
-//     }
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else {
+        console.log(i);
+    }
 
-// }
+}
 
 
 // Задание 6
 // Перепишите код, заменив цикл for на while, без изменения поведения цикла.
 
 // for (let i = 0; i < 3; i++) {
-// alert( `number ${i}!` );
-// }
-
-// let i = 0;
-// while (i < 3) {
 //     alert(`number ${i}!`);
-//     i++;
-
 // }
+
+let i = 0;
+while (i < 3) {
+    alert(`number ${i}!`);
+    i++;
+
+}
 
 
 // Задание 7
 // Переписать задание 4 в виде функции. Т.е. описать функцию, которая выводит квадраты чисел из заданного диапазона (диапазон будет попадать в функцию как аргументы).
 
-
-// const calcSquare = (min, max) => {
-//     for (let i = min; i < max; i++) {
-//         console.log(i ** 2);
-//     }
-// }
-// calcSquare(3, 10);
-
+const calcSquare = (min, max) => {
+    for (let i = min; i < max; i++) {
+        console.log(i ** 2);
+    }
+}
+calcSquare(3, 10);
 
 
 // Задание 8
@@ -105,9 +103,7 @@
 // return Math.floor(Math.random() * (max - min)) + min;
 // }
 // Подсказка: 
-
 // Каждый цвет - 3 числа, каждое число лежит в диапазоне от 0 до 255. 
-
 // Одна функция может вызывать другую функцию
 
 
@@ -134,3 +130,28 @@ function getRandomInteger(min, max) {
 }
 
 console.log(getRandomRGB());
+
+
+// Задание 9
+// Написать функцию, которая принимает целое число n.   
+// Внутри функции запустить цикл от 1 до n с шагом 0.5. На каждой итерации цикла выводить в консоль текущее число (i) и информацию о том, целое ли оно.
+// Такая функция не будет ничего возвращать, она просто выводит информацию в консоль.
+// Пример, при запуске функции с параметром 3:
+// "1 integer"
+// "1.5 decimal"
+// "2 integer"
+
+// "2.5 decimal"
+
+const typeNumber = (n) => {
+    for (let i = 1; i < n; i = i + 0.5) {
+
+        if (i % 1 === 0) {
+            console.log(`${i} integer`);
+        } else {
+            console.log(`${i} decimal`);
+        }
+    }
+}
+
+typeNumber(5);
