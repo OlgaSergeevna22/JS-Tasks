768e0176ca450ff43f7a2f254d934992db0752a6
Olga Geres <olgageres13@gmail.com>
2022-12-10 01:11:24 +0300

    change name function

--- 2js.js
+++ 2js.js
@@ -133,7 +133,6 @@ console.log(getRandomRGB());
 console.log(getRandomRGB1());
 
 
-
 // Задание 9
 // Написать функцию, которая принимает целое число n.   
 // Внутри функции запустить цикл от 1 до n с шагом 0.5. На каждой итерации цикла выводить в консоль текущее число (i) и информацию о том, целое ли оно.
@@ -145,7 +144,7 @@ console.log(getRandomRGB1());
 
 // "2.5 decimal"
 
-const typeNumber = (n) => {
+const showTypeNumber = (n) => {
     for (let i = 1; i < n; i = i + 0.5) {
 
         if (i % 1 === 0) {
