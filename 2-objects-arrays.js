1025d28afe55c14e640a415b3bf1f578c4cf179f
Olga Geres <olgageres13@gmail.com>
2022-12-13 00:42:08 +0300

    Task 5

--- 2-objects-arrays.js
+++ 2-objects-arrays.js
@@ -81,3 +81,37 @@ for (let i = 0; i < numbers.length; i++) {
 
     console.log(`Индексу ${[i]} соответствует число ${numbers[i]}`);
 }
+
+
+// Задание 5
+// Дан массив объектов, например:
+// let questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+//     corAnswer: 0
+// }];
+// Добавить в каждый объект дополнительное поле usersAnswer со значением null.
+// Решение должно работать для массива любой длины.
+
+let questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+
+
+for (let i = 0; i < questions.length; i++) {
+
+    questions[i].usersAnswer = null;
+
+}
+
+console.log(questions);
