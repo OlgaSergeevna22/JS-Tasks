c8fc8af386c8c4433c481867cb174b8788e1ae4c
Olga Geres <olgageres13@gmail.com>
2022-12-13 20:32:54 +0300

    Task 10

--- 2-objects-arrays.js
+++ 2-objects-arrays.js
@@ -224,3 +224,28 @@ for (let item of users) {
         console.log(item.name);
     }
 }
+
+
+// Задание 10*
+// Задать массив слов. Например:
+// let vegetables = ["морковь", "баклажан", "репа", "топинамбур"];
+// 1) В цикле сформировать массив объектов с ключами word (само слово), length (длина слова):
+// [{word:'морковь', length: 7}, {word:'баклажан', length: 8} и т.п.]
+// Вывести этот массив в консоль.
+// Подсказка: длину строки можно определить с помощью метода .length. Например, для переменной let string = 'text' это будет string.length (результат - 4, т.к. слово text состоит из 4 символов)
+// 2) Пройтись по полученному массиву объектов и вывести в консоль строки вида "слово - длина_слова", например "картошка - 8"
+
+let vegetables = ["морковь", "баклажан", "репа", "топинамбур"];
+let vegetablesObjects = [];
+
+for (let item of vegetables) {
+
+    vegetablesObjects.push({ word: item, length: item.length });
+}
+
+console.log(vegetablesObjects);
+
+for (let item of vegetablesObjects) {
+
+    console.log(`${item.word} - ${item.length}`);
+}
