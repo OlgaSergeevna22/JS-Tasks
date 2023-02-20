21131901d544917d49b9a969b7a7df2051ca1c26
Olga Geres <olgageres13@gmail.com>
2023-02-20 17:13:23 +0300

    task 4*

--- lesson8.js
+++ lesson8.js
@@ -44,13 +44,42 @@
 // console.log(counter2());
 
 
-// 3. Написать функцию startsWith, которая принимает 2 строки и проверяет, начинается ли первая строка с символов второй строки. Функция возвращает true или false.
+// // 3. Написать функцию startsWith, которая принимает 2 строки и проверяет, начинается ли первая строка с символов второй строки. Функция возвращает true или false.
 
-function startsWith(str1, str2) {
-    str1 = str1.split('');
-    str2 = str2.split('')
-    return (str2.includes(str1[0]));
+// function startsWith(str1, str2) {
+//     str1 = str1.split('');
+//     str2 = str2.split('')
+//     return (str2.includes(str1[0]));
+// }
+
+// console.log(startsWith('asdf', 'dfg'));
+// console.log(startsWith('qwert', 'qazxs'));
+
+
+// 4* Реализовать функционал модального окошка, которое открывается с затемнением фона при нажатии на кнопку и закрывается по нажатию на кнопку-крестик или на тёмную область вокруг окошка.
+
+let popup = document.getElementById('myPopup');
+let btnOpen = document.querySelector('.btnPoput');
+let btnClose = document.querySelector('.close');
+let windowDark = document.querySelector('.windowDark')
+let overlay = document.querySelector('.overlay')
+
+function open() {
+    popup.style.display = 'block';
+    overlay.style.display = 'block'
 }
+btnOpen.addEventListener('click', open)
 
-console.log(startsWith('asdf', 'dfg'));
-console.log(startsWith('qwert', 'qazxs'));
+function close() {
+    popup.style.display = 'none'
+    overlay.style.display = 'none'
+}
+btnClose.addEventListener('click', close)
+
+function closeDark(event) {
+    if (event.target == overlay) {
+        popup.style.display = 'none'
+        overlay.style.display = 'none'
+    }
+}
+window.addEventListener('click', closeDark)
